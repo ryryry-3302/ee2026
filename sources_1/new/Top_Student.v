1bd5b6a5284d205db9b2ce615c76830f65a8ceb0
flamerten <flamerten@gmail.com>
2024-03-20 01:12:03 +0800

    Add sw[5] as a way to activate group task, else black oled

--- sources_1/new/Top_Student.v
+++ sources_1/new/Top_Student.v
@@ -13,7 +13,7 @@
 
 module Top_Student (
     input clk,                //100MHz
-    input [15:0] sw,          //0,1,2,4 and 13,14,15 used
+    input [15:0] sw,          //SW0 (Tasks) SW1,2,3,4 (ABCD) SW5 GRP
     input btnC, btnL, btnR, btnU, btnD,
     output [4:0] led,         //1234 for subtask ABCD
     output [7:0] JC, 
@@ -24,7 +24,7 @@ module Top_Student (
 );
 
     //OLED Driver -----------------------------------
-    wire [15:0] oled_colour;
+    reg [15:0] oled_colour;
     wire frame_begin;
     wire [12:0] pixel_index;
     wire sending_pixels, sample_pixel;
@@ -34,17 +34,16 @@ module Top_Student (
                          .COUNT_STOP(32'd7),
                          .CLOCK_OUT(CLK_6MHz25));
                           
-    //celebrateionnnnn!!!!!!!!
     //Celebration Module -----------------------------------
     wire [15:0] celebration_oled_colour;
-
+    reg celebrationState = 0;
     congrats_modules my_congrats (.clk(clk), .celebrationState(celebrationState), .pixel_index(pixel_index), .oled_data(celebration_oled_colour));
 
     //Mouse Driver---------------------------------------
     wire [11:0] xpos; wire [11:0] ypos; wire [3:0] zpos;
     wire newevent;
     wire left_click, centre_click, right_click;
-    reg reset; //used for paint
+    reg reset_canvas;
 
     //---------------------------------------------------
 
@@ -57,25 +56,23 @@ module Top_Student (
     CustomClock clk12mhz5(.CLOCK_IN(clk),
                           .COUNT_STOP(3),
                           .CLOCK_OUT(CLK_12MHz5)); 
-    wire CLK_10KHz; //10kHz                    
+    wire CLK_10KHz;                 
     CustomClock clk10khz(.CLOCK_IN(clk),
                          .COUNT_STOP(32'd5000 - 1),
                          .CLOCK_OUT(CLK_10KHz));
 
     wire [15:0] LED_Num_Paint;
-    reg [3:0] Num_Detected;
-    
+    reg [3:0] Num_Detected = 15;
     wire [6:0] paint_v_seg; //Dummy wire var
     wire [15:0] paint_colour_chooser;
     //---------------------------------------------------
     
     //Task Switching
-    //wire taskb_task_active;
-    //assign taskb_task_active = sw[2];
-    reg task_a_active = 0;
-    reg task_b_active = 1'b0;
-    reg task_c_active = 0;
-    reg task_d_active = 0;
+    reg task_a_active   = 0;
+    reg task_b_active   = 0;
+    reg task_c_active   = 0;
+    reg task_d_active   = 0;
+    reg task_grp_active = 0;
     
     assign led[0] = task_a_active;
     assign led[1] = task_b_active;
@@ -84,10 +81,29 @@ module Top_Student (
     
     always@(posedge CLK_10KHz)
     begin
-        task_a_active = sw[1];
-        task_b_active = sw[2] && !sw[1];
-        task_c_active = sw[3] && !sw[2:1];
-        task_d_active = sw[4] && !(sw[3:1]);
+        task_a_active <= 0;
+        task_b_active <= 0;
+        task_c_active <= 0;
+        task_d_active <= 0;
+        task_grp_active <= 0;
+        
+        if(sw[1])
+            task_a_active   <= 1;
+        else if(sw[2])
+            task_b_active   <= 1;
+        else if(sw[3])
+            task_c_active   <= 1;
+        else if(sw[4])
+            task_d_active   <= 1;
+        else if(sw[5])
+            task_grp_active <= 1;                            
+  
+        /*
+        task_a_active   = sw[1];
+        task_b_active   = sw[2] && !sw[1];
+        task_c_active   = sw[3] && !sw[2:1];
+        task_d_active   = sw[4] && !(sw[3:1]);
+        task_grp_active = sw[5] && !(sw[4:1]);*/
     end
     
     wire [15:0] oled_coloura;
@@ -103,12 +119,23 @@ module Top_Student (
     
     subtaskDonut taskD( .enable(task_d_active),.clk(clk),.sw(sw[0]),.btnC(btnC),.btnU(btnU),.btnR(btnR), .btnL(btnL),.btnD(btnD), .currentPixel(pixel_index), .pixelOutput(oled_colourd));
     
-    assign oled_colour = task_a_active? oled_coloura:
-     task_b_active? oled_colourb:
-     task_c_active? oled_colourc:
-     task_d_active? oled_colourd:
-     celebrationState? celebration_oled_colour:paint_colour_chooser;
-
+    always@(*)
+    begin
+        if(task_a_active)
+            oled_colour = oled_coloura;
+        else if(task_b_active)
+            oled_colour = oled_colourb;
+        else if(task_c_active)
+            oled_colour = oled_colourc;
+        else if(task_d_active)
+            oled_colour = oled_colourd;
+        else if(celebrationState)
+            oled_colour = celebration_oled_colour;
+        else if(task_grp_active) //Paint.v
+            oled_colour = paint_colour_chooser;
+        else
+            oled_colour = 16'h0000; //Force it to be white first
+    end
 
     //7 seg control--------------------------------------------------
    
@@ -148,39 +175,42 @@ module Top_Student (
     
     always @ (posedge clk) 
     begin 
-        if ( task_a_active || task_b_active || task_c_active || task_d_active )
+        if ( task_a_active || task_b_active || task_c_active || task_d_active || ~task_grp_active)
             begin
-            en <= 0 ;
-            reset <= 1;
+            en <= 0 ; //Turn seg if task active, or the grp task is not active
+            reset_canvas <= 1; //Reset canvas if any task is active or grp task not active
+            anode_1 <= 15; anode_0 <= 15;  //clear all anodes
+            anode_3 <= 15; anode_2 <= 15;   //4.E4     
             end
         else
             begin
             en <= 1;
-            reset <= right_click;
+            reset_canvas <= right_click;
             end
-        if (!sw[15] && !sw[14] && !sw[13])
-            begin 
-            anode_1 <= 0; anode_0 <= 6;   //Grp Number
-            anode_3 <= 5; anode_2 <= 3;  //5, Wed PM
+            
+        
+        if(sw[15]) //AN3, AN2, AN0 not affected
+            begin
+            anode_1 <= Num_Detected;  //AN1 > Paint,v
             end
-        else if (!sw[15] && !sw[14] && sw[13]) 
-            begin  
-            anode_1 <= 15; anode_0 <= 15;  //Off
-            anode_3 <= 5;  anode_2 <= 3;   //4.E4
-            end
-        else if(!sw[15] && sw[14])  //AN3, AN2, AN1 not affected
+        else if(sw[14]) //AN3, AN2, AN1 not affected
             begin
             anode_0 <= Num_Detected; //AN0 > Paint,v
             end
-        else if(sw[15])  //AN3, AN2, AN0 not affected
+        else if(sw[13])
             begin
-            anode_1 <= Num_Detected;  //AN1 > Paint,v
+            anode_1 <= 15; anode_0 <= 15;  //Off
+            anode_3 <= 5;  anode_2 <= 3;   //4.E4            
+            end
+        else
+            begin
+            anode_1 <= 0; anode_0 <= 6;   //Grp Number
+            anode_3 <= 5; anode_2 <= 3;  //5, Wed PM            
             end
     end
     //--------------------------------------------------
     
     //Celebration detection ----------------------------
-    reg celebrationState = 0;
     always @ (posedge clk)
     begin
         if (anode_0 ==  6 && anode_1 == 0 && btnC && en) begin
@@ -227,16 +257,14 @@ module Top_Student (
         .ps2_clk(PS2Clk),
         .ps2_data(PS2Data));
 
-
-    
-    
+  
     paint paint_mod(
         .clk_100M(clk),
         .clk_25M(CLK_25MHz),
         .clk_12p5M(CLK_12MHz5),
         .clk_6p25M(CLK_6MHz25),
         .slow_clk(CLK_10KHz),
-        .mouse_l(left_click), .reset(reset), .enable(1),  
+        .mouse_l(left_click), .reset(reset_canvas), .enable(1),  
         .mouse_x(xpos), .mouse_y(ypos),
         .pixel_index(pixel_index),
         .led(LED_Num_Paint),       
